e75268e0907adf21e97d608f5760b41db565c854
Ivan <ivan@example.com>
2020-10-14 19:00:07 -0400

    Add login with pwd comparison

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
 const bcrypt = require('bcryptjs');
-const { User, validateSignupInfo } = require('../models/User');
+const { User, validateSignupInfo, validateLoginInfo } = require('../models/User');
 
 router.post('/signup', async (req, res) => {
-    const error = await validateSignupInfo(req.body);
-    if (error) return res.status(400).send(`Unable to parse input object. \nReason: ${error}`);
+    const validationError = await validateSignupInfo(req.body);
+    if (validationError) return res.status(400).send(`Unable to parse input object. \nReason: ${validationError}`);
 
     const userExists = await User.findOne({ email: req.body.email });
     if (userExists) return res.status(400).send('User with such email already exists.');
@@ -25,6 +25,19 @@ router.post('/signup', async (req, res) => {
         .catch((error) => {
             return res.status(400).send(`Unable to save User in db. \nReason: ${error.message}`);
         });
+});
+
+router.post('/login', async (req, res) => {
+    const validationError = await validateLoginInfo(req.body);
+    if (validationError) return res.status(400).send(`Unable to parse input object. \nReason: ${validationError}`);
+
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) return res.status(400).send('Incorrect email or password.');
+
+    const isValid = await bcrypt.compare(req.body.password, user.password);
+    if (!isValid) return res.status(400).send('Incorrect email or password.');
+
+    return res.status(200).send(`Welcome ${user.name}`);
 });
 
 module.exports = router;
