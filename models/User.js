551a75c0b83706e5f9aabd8fb5820658aa157ec7
Ivan <ivan@example.com>
2020-10-14 18:51:00 -0400

    Add bcryptjs; hashed password

--- models/User.js
+++ models/User.js
@@ -22,11 +22,33 @@ const dbSchema = new mongoose.Schema({
     }
 });
 
-const userSchema = joi.object({
-    name: joi.string().min(3).max(255).required(),
-    email: joi.string().min(3).max(255).email().required(),
-    password: joi.string().min(6).max(1024).required()
-});
+const validateSignupInfo = async (data) => {
+    const userSignupSchema = joi.object({
+        name: joi.string().min(3).max(255).required(),
+        email: joi.string().min(3).max(255).email().required(),
+        password: joi.string().min(6).max(1024).required()
+    });
+
+    try {
+        await userSignupSchema.validateAsync(data);
+    } catch (error) {
+        return error;
+    }
+};
+
+const validateLoginInfo = async (data) => {
+    const userLoginSchema = joi.object({
+        email: joi.string().min(3).max(255).email().required(),
+        password: joi.string().min(6).max(1024).required()
+    });
+
+    try {
+        await userLoginSchema.validateAsync(data);
+    } catch (error) {
+        return error;
+    }
+};
 
 module.exports.User = mongoose.model('User', dbSchema);
-module.exports.userSchema = userSchema;
+module.exports.validateSignupInfo = validateSignupInfo;
+module.exports.validateLoginInfo = validateLoginInfo;
--- routes/auth.js
+++ routes/auth.js
@@ -1,16 +1,21 @@
 const router = require('express').Router();
-const { User, userSchema } = require('../models/User');
+const bcrypt = require('bcryptjs');
+const { User, validateSignupInfo } = require('../models/User');
 
 router.post('/signup', async (req, res) => {
-    userSchema.validateAsync(req.body)
-        .catch((error) => {
-            return res.status(400).send(`Unable to parse input object. \nReason: ${error}`);
-        });
+    const error = await validateSignupInfo(req.body);
+    if (error) return res.status(400).send(`Unable to parse input object. \nReason: ${error}`);
+
+    const userExists = await User.findOne({ email: req.body.email });
+    if (userExists) return res.status(400).send('User with such email already exists.');
+
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
 
     const user = new User({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password
+        password: hashedPassword
     });
 
     user.save()
@@ -18,7 +23,7 @@ router.post('/signup', async (req, res) => {
             return res.status(200).send(result);
         })
         .catch((error) => {
-            return res.status(400).send(`Unable to save User in db. \nReason: ${error.message}`)
+            return res.status(400).send(`Unable to save User in db. \nReason: ${error.message}`);
         });
 });
 
