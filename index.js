a9f1c740ad908842b505f8526840f3894f0feaf1
Ivan <ivan@example.com>
2020-10-14 21:46:12 -0400

    Add ENV variables check

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
-require("dotenv").config();
-
 const express = require("express");
 const mongo = require("mongoose");
-const app = express();
 const authRoute = require("./routes/auth");
 const privateRoute = require("./routes/private");
+const envVariables = require("./middleware/envVariables");
 
+const app = express();
 app.use(express.json());
 
-app.use("/api/user", authRoute);
 app.use("/", privateRoute);
+app.use("/api/user", authRoute);
+
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+envVariables.check([
+  "PORT",
+  "MONGODB_CONNECTIONSTRING",
+  "JWT_SECRET",
+  "JWT_TTL",
+]);
 
 mongo
   .connect(process.env.MONGODB_CONNECTIONSTRING, {
@@ -18,7 +27,9 @@ mongo
   })
   .then(() => console.log("Connection with the database has been established"))
   .catch((error) =>
-    console.log(`Could not establish connection. \nReason: ${error.message}`)
+    console.log(`Could not establish connection.\nReason: ${error.message}`)
   );
 
-app.listen(process.env.APP_PORT, () => console.log("App is running."));
+app.listen(process.env.PORT, () =>
+  console.log(`App is running on PORT ${process.env.PORT}`)
+);
